c31ebbb369848799a9bf5d1411ad8bf9778b8799
Kredam <akitlosz2000@gmail.com>
2021-04-06 19:41:21 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+    }
+}
+    
